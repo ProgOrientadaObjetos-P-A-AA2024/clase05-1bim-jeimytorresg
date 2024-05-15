f10513efa7bf716afc838733308d15f635afe1c0
Jeimy Torres <jatorres99@utpl.edu.ec>
2024-05-15 16:00:58 -0500

    cambios ejemplo

---
+++ EjemplosArreglos/EjercicioArreglosPOO/src/paquete02/Ejecutor2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete02;
+
+import paquete01.LibretaCalificacion;
+
+/**
+ *
+ * @author utpl
+ */
+public class Ejecutor2 {
+
+    public static void main(String[] args) {
+        double[] notas = {10, 9, 8, 10};
+        String nombreEstudiante = "Rene Elizalde";
+        LibretaCalificacion estudiante = new LibretaCalificacion(nombreEstudiante,
+                notas);
+        estudiante.establecerPromedio();
+        estudiante.establecerPromedioCualitativo();
+        
+        System.out.printf("%s\n", estudiante);
+    }
+}
